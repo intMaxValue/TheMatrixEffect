3703356ff84e19c5fd308c5d87fb46b1bcd75d4b
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-02-17 12:23:41 +0200

    Initial commit

---
+++ TheMatrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WindowHeight = Console.BufferHeight = 50;
+        Console.WindowWidth = Console.BufferWidth = 200;
+
+        Random rand = new Random();
+
+        while (true)
+        {
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.SetCursorPosition(i, rand.Next(Console.WindowHeight));
+                Console.Write((char)('!' + rand.Next(94)));
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.SetCursorPosition(i, rand.Next(Console.WindowHeight));
+                Console.Write((char)('!' + rand.Next(94)));
+            }
+
+            Thread.Sleep(100);
+        }
+    }
+}
